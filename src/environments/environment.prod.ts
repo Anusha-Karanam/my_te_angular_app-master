79ff7b813871ca75fd9ad7311eebbd1a850b3877
Anusha-Karanam <anushakaranambest@gmail.com>
2022-10-18 17:10:06 +0530

    commit18/10/22

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  userapi:"https://heroku-json-server-walmarttt.herokuapp.com/users",
+  // userapi:"https://heroku-json-server-walmarttt.herokuapp.com/users",
+  userapi:"https://localhost:7233/api/Registrations",
   cartapi:"https://heroku-json-server-walmarttt.herokuapp.com/cart",
   postsapi:"https://heroku-json-server-walmarttt.herokuapp.com/posts"
 };
